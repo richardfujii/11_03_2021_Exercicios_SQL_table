58a276d39775a366281656d60a5ff7025daea70b
richardfujii <richardcf1998@gmail.com>
2021-03-11 18:12:10 -0300

    Exercício 2

---
+++ exercicio2.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE db_sapataria;
+
+USE db_sapataria;
+
+CREATE TABLE tb_produto(
+	id BIGINT(2) AUTO_INCREMENT,
+	nome VARCHAR(60),
+    cor VARCHAR(60),
+    quantidade INTEGER,
+    preco REAL,
+    presente BOOLEAN,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO tb_produto (nome, cor, quantidade, preco, presente) VALUES ("Nike", "azul", 3, 400.89, true);
+INSERT INTO tb_produto (nome, cor, quantidade, preco, presente) VALUES ("PUMA", "cinza", 5, 700.88, true);
+INSERT INTO tb_produto (nome, cor, quantidade, preco, presente) VALUES ("ADIDAS", "vermelho", 2, 300.36, true);
+INSERT INTO tb_produto (nome, cor, quantidade, preco, presente) VALUES ("REBOOK", "verde", 8, 200.08, true);
+INSERT INTO tb_produto (nome, cor, quantidade, preco, presente) VALUES ("VANS", "roxo", 3, 200.13, true);
+
+SELECT *FROM tb_produto;
+
+ALTER TABLE tb_produto
+DROP presente;
+
+UPDATE tb_produto SET preco = 500.00 WHERE id=1;
+UPDATE tb_produto SET preco = 1000.00 WHERE id=2;
+UPDATE tb_produto SET preco = 2000.00 WHERE id=3;
+
+SELECT *FROM tb_produto;
